678dc3191c6326594785cd544a46dd2428e03488
DOSull <david.osullivan@vuw.ac.nz>
2020-07-13 20:17:34 +1200

    bit of a tidy up moving junk DNA to the end of the code - nothing deleted, just cleared to one side

--- what3chords-app/js/app-es6.js
+++ what3chords-app/js/app-es6.js
@@ -1,10 +1,5 @@
 "use strict";
-
 const WEB_MERC_LIMIT = 85.051129;
-const LON_RES = 130400;
-const LAT_RES = 65200;
-const CAT_SHUFFLES = 15;
-const N_CHORDS_LL = 2141;
 
 const USE_H3 = true;
 const N_CHORDS_H3 = 1692;
@@ -69,7 +64,9 @@ function switchTheme() {
   window.location = window.location.href.split("?")[0] + `?theme=${THEME}`;
 }
 
-
+// -----------------------------------
+// Geocoding stuff
+// -----------------------------------
 function GetMyLocationSong(pos) {
     var crd = pos.coords;
     console.log("Found geolocation. ",crd.longitude," ",crd.latitude);
@@ -138,7 +135,6 @@ $.when(
 });
 
 
-
 function getSamples() {
   // passing a single instrument name loads one instrument and returns the tone.js object
   GUITAR = SampleLibrary.load({
@@ -151,8 +147,6 @@ function getSamples() {
   document.body.append(GUITAR); // could this fix the iOS problem?
 }
 
-
-// let ALL_THE_NOTES = {};
 let CHORDS = [];
 function processChordsData() {
   let chordsFlattened = {};
@@ -216,12 +210,13 @@ function render() {
 };
 
 
+// build the tooltip
 function setTooltip(object, x, y, c) {
   let el = document.getElementById("tooltip");
   if (object) {
     // This makes a H3 hex and puts it in the console but
     // no idea how to inject it into a layer
-    let hex = h3.h3ToGeoBoundary(h3.geoToH3(c[1], c[0], H3_RES));
+    // let hex = h3.h3ToGeoBoundary(h3.geoToH3(c[1], c[0], H3_RES));
     let h3c = getH3Code(c);
     let abc = getCode(c);
     let codeToUse =  USE_H3 ? h3c : abc;
@@ -257,6 +252,212 @@ function getChordTableRow(c) {
   <td>${c.fingers}</td>
   </tr>`;
 }
+
+
+function isValidH3Code(bits) {
+  // returns false if a bitswap has resulted in an invalid h3 code, true otherwise.
+
+  // check first seven bits are valid h3 region codes between 0-121
+  var regionCode = parseInt(bits.slice(0, 7), 2);
+  if ((regionCode) < 0 || (regionCode) > 121) {
+    return false
+  };
+  for (let res = 8; res <= H3_RES; res += 3) {
+  // for (let res = 1; res <= H3_RES; res++) {
+    if (parseInt(bits.slice(res, res + 3), 2) == 7) {
+    // if (parseInt(bits.slice((res - 1) * 3 + 8, (res) * 3 + 8), 2) == 7) {
+      return false
+    }
+  }
+  return true;
+}
+
+
+function getH3Code(c) {
+  // use the homebrew base X function to convert the decimal
+  // H3 index into base 1692 to index into the chord array
+  let h3Code = nBaseX(
+    h3ToDecimal(
+      h3.geoToH3(c[1], c[0], H3_RES)
+    ),
+    N_CHORDS_H3);
+  // console.log(inverseH3Code(h3Code));
+  return h3Code;
+}
+
+// recover the 34 bits of the H3 code that index level H3_RES
+// starting from the 3 chord array indices
+function inverseH3Code(c3) {
+  // retrieve the decimal index
+  let index = c3[0] * N_CHORDS_H3 * N_CHORDS_H3 + c3[1] * N_CHORDS_H3 + c3[2];
+  let bits = "";
+  // the rightmost H3_RES digits are 7-ary digits encode as 3 bits
+  for (let i = 0; i < H3_RES; i++) {
+    bits = ((index % 7).toString(2).padStart(3, "0")) + bits;
+    index = Math.floor(index / 7);
+  }
+  // leftmost 7 digits are those remaining
+  bits = index.toString(2).padStart(7, "0") + bits;
+
+  // inverse any bitswapping
+  return bitswapInverse(bits,BITSWAP);
+}
+
+// idx is the raw 64 bit H3 level H3_RES index
+// we need bits 12 to 46
+// first 7 bits are high level 'region'
+// the remaining 27 bits are H3_RES sets of base 7 hierarchical indexing
+function h3ToDecimal(idx) {
+  // first left pad to 16 digits with 0s
+  let idxPad = idx.padStart(16, 0);
+  // chop it into 4 hex digit pieces to avoid issues with overflow in parseInt
+  let bin = parseInt(idxPad.slice(0, 4), 16).toString(2).padStart(16, "0") +
+            parseInt(idxPad.slice(4, 8), 16).toString(2).padStart(16, "0") +
+            parseInt(idxPad.slice(8, 12), 16).toString(2).padStart(16, "0") +
+            parseInt(idxPad.slice(12), 16).toString(2).padStart(16, "0");
+  H3_CODE.prefix = bin.slice(0, 12);
+  H3_CODE.suffix = bin.slice(H3_CODE.lastSigBit);
+  // extract the bits we need
+  bin = bin.slice(12, H3_CODE.lastSigBit);
+  // do any bitswapping
+  bin = bitswap(bin,BITSWAP);
+  // console.log(bin);
+  // the power of 7 we are currently working on
+  let pow = H3_RES;
+  // first 7 bits are region
+  let result = parseInt(bin.slice(0, 7), 2) * (7 ** pow);
+  // remaining bits will be sliced 3 at a time into base-7 digits
+  let heptDigits = bin.slice(7);
+  while (pow > 0) {
+    pow--; // decrement the power
+    // add the next 7-digit to the results
+    result = result + parseInt(heptDigits.slice(0, 3), 2) * (7 ** pow);
+    // move to the next digit
+    heptDigits = heptDigits.slice(3);
+  }
+  return result;
+}
+
+
+function nBaseX(n, x, alphabet) {
+  let result = [];
+  let digitsRequired = Math.floor(Math.log(n) / Math.log(x)) + 1;
+  let q = n;
+  for (let i = 0; i < digitsRequired; i++) {
+    result.unshift(q % x);
+    q = Math.floor(q / x);
+  }
+  // console.log(result);
+  if (alphabet) {
+    return result.map(digit => alphabet[digit]).join("");
+  } else {
+    return result;
+  }
+}
+
+// ----------------------------------------
+// BRING THA NOIZ
+// ----------------------------------------
+const DIST = new Tone.Distortion(0.25).toMaster();
+
+function playChord(notes1, notes2, notes3) {
+  let chords = [];
+  chords.push(notes1.map(x => Tone.Midi(x).toNote()));
+  chords.push(notes2.map(x => Tone.Midi(x).toNote()));
+  chords.push(notes3.map(x => Tone.Midi(x).toNote()));
+  chords.push(notes1.map(x => Tone.Midi(x).toNote()));
+
+  let now = Tone.now()
+  let bpm = 180;
+  let durn = 0.5 * 60 / bpm;
+  // see https://en.wikipedia.org/wiki/Strum#Strumming_patterns
+  // and https://rockguitaruniverse.com/guitar-strumming-patterns/#The_Ramones_Pattern
+  let pattern = "dudud-d-";
+  for (let i = 1; i <= chords.length; i++) {
+    let c = chords[i - 1];
+    // 4:4 time strummed up and down, missing the strum where pattern is -
+    if (i < chords.length) {
+      for (let p of pattern) {
+        if (p != "-") {
+          strumChord(GUITAR, c, now, 0.01, 3 * durn);
+        }
+        now = now + durn;
+        c.reverse(); // to get down/up strums
+      }
+      now = now + 0.0;
+    } else {
+      // last time, just play it once
+      strumChord(GUITAR, c, now, 0.01, 4 * durn);
+    }
+  }
+}
+
+// crude attempt to strum chord, not just play all strings at once
+function strumChord (instrument, notes, now, gap, duration) {
+  let t = now;
+  for (let n of notes) {
+    instrument.triggerAttackRelease(n, duration, t).connect(DIST);
+    t = t + gap;
+  }
+}
+
+function notesToFreq(n) {
+  return n.map(x => Tone.Midi(x).toFrequency());
+}
+
+
+// --------------------------------------
+// THE DNA JUNKYARD
+// --------------------------------------
+const LON_RES = 130400;
+const LAT_RES = 65200;
+const CAT_SHUFFLES = 15;
+const N_CHORDS_LL = 2141;
+
+// get a 3 digit sequence of 0..2140 indices into the Array of chords
+function getCode(c) {
+  // get x and y values that are integers 0..130400, 0..65200
+  let xy = [
+    Math.round(rescale(c[0], -180, 180, 0, LON_RES)),
+    Math.round(rescale(c[1], -WEB_MERC_LIMIT, WEB_MERC_LIMIT, 0, LAT_RES))
+  ];
+  // rescale to unit square
+  xy = [
+    rescale(xy[0], 0, LON_RES, 0, 1),
+    rescale(xy[1], 0, LAT_RES, 0, 1)
+  ]
+  // shuffle them
+  xy = doTheShuffle(xy, CAT_SHUFFLES);
+  // round back to ints
+  let x = Math.round(rescale(xy[0], 0, 1, 0, LON_RES));
+  let y = Math.round(rescale(xy[1], 0, 1, 0, LAT_RES));
+
+  // convert to an index
+  let i = x + y * LON_RES;
+  return nBaseX(i, N_CHORDS_LL);
+}
+
+
+// rescale x from xmin-xmax to mn-mx
+function rescale(x, xmin, xmax, mn, mx) {
+  return mn + (mx - mn) * (x - xmin) / (xmax - xmin);
+}
+
+
+// shuffle the lon lat in a unit square using Arnold's Cat, see
+// https://en.wikipedia.org/wiki/Arnold's_cat_map
+function doTheShuffle(c, n) {
+  let xy = c;
+  for (let i = 0; i < n; i ++) {
+    xy = arnoldsCat(xy);
+  }
+  return xy;
+}
+
+function arnoldsCat(xy) {
+  return [(2 * xy[0] + xy[1]) % 1, (xy[0] + xy[1]) % 1];
+}
+
 
 // Competing scrambler
 // a dictionary of forward bit swaps of the 34 bits from
@@ -330,7 +531,7 @@ function bitswap(bits, BITSWAPlist) {
   var swappedbits = bits;
   for(var currSwapPos = 0; currSwapPos < BITSWAPlist.length; currSwapPos++) {
     var candidatebits = swapbits(BITSWAPlist[currSwapPos],swappedbits);
-    if (checkH3Validity(candidatebits)) {
+    if (isValidH3Code(candidatebits)) {
       swappedbits = candidatebits;
     }
   }
@@ -339,199 +540,4 @@ function bitswap(bits, BITSWAPlist) {
 
 function bitswapInverse(bits, BITSWAPlist) {
   return bitswap(bits, BITSWAPlist.reverse());
-}
-
-function checkH3Validity(bits) {
-  // returns false if a bitswap has resulted in an invalid h3 code, true otherwise.
-
-  // check first seven bits are valid h3 region codes between 0-121
-  var regionCode = parseInt(bits.slice(0, 8), 2);
-  if ((regionCode) < 0 || (regionCode) > 121) {
-    return false
-  };
-  for(var res = 1; res <= H3_RES; res++) {
-    if (parseInt(bits.slice((res-1) * 3 + 8, (res) * 3 + 8), 2) == 7) {
-      return false
-    }
-  }
-  return true;
-}
-
-
-
-
-
-function getH3Code(c) {
-  // use the homebrew base X function to convert the decimal
-  // H3 index into base 1692 to index into the chord array
-  let h3Code = nBaseX(
-    h3ToDecimal(
-      h3.geoToH3(c[1], c[0], H3_RES)
-    ),
-    N_CHORDS_H3);
-  // console.log(inverseH3Code(h3Code));
-  return h3Code;
-}
-
-// recover the 34 bits of the H3 code that index level H3_RES
-// starting from the 3 chord array indices
-function inverseH3Code(c3) {
-  // retrieve the decimal index
-  let index = c3[0] * N_CHORDS_H3 * N_CHORDS_H3 + c3[1] * N_CHORDS_H3 + c3[2];
-  let bits = "";
-  // the rightmost H3_RES digits are 7-ary digits encode as 3 bits
-  for (let i = 0; i < H3_RES; i++) {
-    bits = ((index % 7).toString(2).padStart(3, "0")) + bits;
-    index = Math.floor(index / 7);
-  }
-  // leftmost 7 digits are those remaining
-  bits = index.toString(2).padStart(7, "0") + bits;
-
-  // inverse any bitswapping
-  return bitswapInverse(bits,BITSWAP);
-}
-
-// idx is the raw 64 bit H3 level H3_RES index
-// we need bits 12 to 46
-// first 7 bits are high level 'region'
-// the remaining 27 bits are H3_RES sets of base 7 hierarchical indexing
-function h3ToDecimal(idx) {
-  // first left pad to 16 digits with 0s
-  let idxPad = idx.padStart(16, 0);
-  // chop it into 4 hex digit pieces to avoid issues with overflow in parseInt
-  let bin = parseInt(idxPad.slice(0, 4), 16).toString(2).padStart(16, "0") +
-            parseInt(idxPad.slice(4, 8), 16).toString(2).padStart(16, "0") +
-            parseInt(idxPad.slice(8, 12), 16).toString(2).padStart(16, "0") +
-            parseInt(idxPad.slice(12), 16).toString(2).padStart(16, "0");
-  H3_CODE.prefix = bin.slice(0, 12);
-  H3_CODE.suffix = bin.slice(H3_CODE.lastSigBit);
-  // extract the bits we need
-  bin = bin.slice(12, H3_CODE.lastSigBit);
-  // do any bitswapping
-  bin = bitswap(bin,BITSWAP);
-  // console.log(bin);
-  // the power of 7 we are currently working on
-  let pow = H3_RES;
-  // first 7 bits are region
-  let result = parseInt(bin.slice(0, 7), 2) * (7 ** pow);
-  // remaining bits will be sliced 3 at a time into base-7 digits
-  let heptDigits = bin.slice(7);
-  while (pow > 0) {
-    pow--; // decrement the power
-    // add the next 7-digit to the results
-    result = result + parseInt(heptDigits.slice(0, 3), 2) * (7 ** pow);
-    // move to the next digit
-    heptDigits = heptDigits.slice(3);
-  }
-  return result;
-}
-
-
-// get a 3 digit sequence of 0..2040 indices into the Array of chords
-function getCode(c) {
-  // get x and y values that are integers 0..130400, 0..65200
-  let xy = [
-    Math.round(rescale(c[0], -180, 180, 0, LON_RES)),
-    Math.round(rescale(c[1], -WEB_MERC_LIMIT, WEB_MERC_LIMIT, 0, LAT_RES))
-  ];
-  // rescale to unit square
-  xy = [
-    rescale(xy[0], 0, LON_RES, 0, 1),
-    rescale(xy[1], 0, LAT_RES, 0, 1)
-  ]
-  // shuffle them
-  xy = doTheShuffle(xy, CAT_SHUFFLES);
-  // round back to ints
-  let x = Math.round(rescale(xy[0], 0, 1, 0, LON_RES));
-  let y = Math.round(rescale(xy[1], 0, 1, 0, LAT_RES));
-
-  // convert to an index
-  let i = x + y * LON_RES;
-  return nBaseX(i, N_CHORDS_LL);
-}
-
-function nBaseX(n, x, alphabet) {
-  let result = [];
-  let digitsRequired = Math.floor(Math.log(n) / Math.log(x)) + 1;
-  let q = n;
-  for (let i = 0; i < digitsRequired; i++) {
-    result.unshift(q % x);
-    q = Math.floor(q / x);
-  }
-  // console.log(result);
-  if (alphabet) {
-    return result.map(digit => alphabet[digit]).join("");
-  } else {
-    return result;
-  }
-}
-
-// rescale x from xmin-xmax to mn-mx
-function rescale(x, xmin, xmax, mn, mx) {
-  return mn + (mx - mn) * (x - xmin) / (xmax - xmin);
-}
-
-
-// shuffle the lon lat in a unit square using Arnold's Cat, see
-// https://en.wikipedia.org/wiki/Arnold's_cat_map
-function doTheShuffle(c, n) {
-  let xy = c;
-  for (let i = 0; i < n; i ++) {
-    xy = arnoldsCat(xy);
-  }
-  return xy;
-}
-
-function arnoldsCat(xy) {
-  return [(2 * xy[0] + xy[1]) % 1, (xy[0] + xy[1]) % 1];
-}
-
-
-const dist = new Tone.Distortion(1).toMaster();
-
-function playChord(notes1, notes2, notes3) {
-  let chords = [];
-  chords.push(notes1.map(x => Tone.Midi(x).toNote()));
-  chords.push(notes2.map(x => Tone.Midi(x).toNote()));
-  chords.push(notes3.map(x => Tone.Midi(x).toNote()));
-  chords.push(notes1.map(x => Tone.Midi(x).toNote()));
-
-  let now = Tone.now()
-  let bpm = 180;
-  let durn = 0.5 * 60 / bpm;
-  // see https://en.wikipedia.org/wiki/Strum#Strumming_patterns
-  // and https://rockguitaruniverse.com/guitar-strumming-patterns/#The_Ramones_Pattern
-  let pattern = "dudud-d-";
-  for (let i = 1; i <= chords.length; i++) {
-    let c = chords[i - 1];
-    // 4:4 time strummed up and down, missing the strum where pattern is -
-    if (i < chords.length) {
-      for (let p of pattern) {
-        if (p != "-") {
-          strumChord(GUITAR, c, now, 0.01, 3 * durn);
-        }
-        now = now + durn;
-        c.reverse(); // to get down/up strums
-      }
-      now = now + 0.0;
-    } else {
-      // last time, just play it once
-      strumChord(GUITAR, c, now, 0.01, 4 * durn);
-    }
-  }
-}
-
-
-// crude attempt to strum chord, not just play all strings at once
-function strumChord (instrument, notes, now, gap, duration) {
-  let t = now;
-  for (let n of notes) {
-    instrument.triggerAttackRelease(n, duration, t).connect(dist);
-    t = t + gap;
-  }
-}
-
-
-function notesToFreq(n) {
-  return n.map(x => Tone.Midi(x).toFrequency());
 }
