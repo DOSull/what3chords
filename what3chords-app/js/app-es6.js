9c3106550b74138f1b4ab2505eed7345d79809e6
DOSull <david.osullivan@vuw.ac.nz>
2020-07-16 18:39:46 +1200

    incrementing version numbers

--- what3chords-app/js/app-es6.js
+++ what3chords-app/js/app-es6.js
@@ -174,6 +174,7 @@ function processChordsData() {
         midi: getNotes(variant.positions),
       });
     }
+    v++;
       // for (let i = 0; i < v.positions.length; i++) {
       //   // console.log(v.positions[i].midi);
       //   let position = v.positions[i];
